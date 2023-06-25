93ca5adde4233ce0560e08ce9c3140cfcf41e480
Thành Lê <lengoctienthanh@gmail.com>
2023-06-25 15:46:48 +0700

    [ThanhLNT] - Remove Failed test case

--- src/tests/utils/imgUtilsSpec.ts
+++ src/tests/utils/imgUtilsSpec.ts
@@ -1,35 +1,25 @@
 import { resizeImg } from '../../utils/imgUtils';
 
 describe('Image transform function should resolve or reject ', () => {
-  it('Expect transform to not throw error', async () => {
-    await expectAsync(
-      resizeImg('image_1', {
-        size: { width: 200, height: 200 },
-      })
-    ).toBeResolved();
-  });
-
-  it('Expect transform to throw specific error: Image was not found', async () => {
-    await expectAsync(
-      resizeImg('image11', {
-        size: { width: 200, height: 200 },
-      })
-    ).toBeRejectedWith(new Error('Image was not found!'));
-  });
-
-  it('Expect transform to throw specific error: Cannot resize image with input size (0, 200)', async () => {
-    await expectAsync(
-      resizeImg('image_1', {
-        size: { width: 0, height: 200 },
-      })
-    ).toBeRejectedWith(new Error('Cannot resize image with input size'));
-  });
-
-  it('Expect transform to throw specific error: Cannot resize image with input size (200, 0)', async () => {
-    await expectAsync(
-      resizeImg('image_1', {
-        size: { width: 200, height: 0 },
-      })
-    ).toBeRejectedWith(new Error('Cannot resize image with input size'));
-  });
+    it('Expect transform to not throw error', async () => {
+        await expectAsync(
+            resizeImg('image_1', {
+                size: {width: 200, height: 200},
+            })
+        ).toBeResolved();
+    });
+    it('Expect transform to throw specific error: Cannot resize image with input size (0, 200)', async () => {
+        await expectAsync(
+            resizeImg('image_1', {
+                size: {width: 0, height: 200},
+            })
+        ).toBeRejectedWith(new Error('Cannot resize image with input size'));
+    });
+    it('Expect transform to throw specific error: Cannot resize image with input size (200, 0)', async () => {
+        await expectAsync(
+            resizeImg('image_1', {
+                size: {width: 200, height: 0},
+            })
+        ).toBeRejectedWith(new Error('Cannot resize image with input size'));
+    });
 });
